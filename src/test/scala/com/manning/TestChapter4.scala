295c7a904b5b80eed09a0ba7c91616d30102b31e
Kalimaha <guido.barbaglia@gmail.com>
2016-09-04 15:40:36 +1000

    Refactoring.

--- src/test/scala/com/manning/TestChapter4.scala
+++ src/test/scala/com/manning/TestChapter4.scala
@@ -1,6 +1,6 @@
 package com.manning
 
-import com.manning.Chapter4._
+import com.manning.Chapter4.{ManningNone, _}
 import org.scalatest.FunSpec
 
 class TestChapter4 extends FunSpec {
@@ -10,32 +10,26 @@ class TestChapter4 extends FunSpec {
     val s1 = ManningSome(42)
 
     describe("map") {
+      val actual = s1 map (a => a.toString)
 
-      it("converts the type according to the given function") {
-        assert(ManningOption.map(s1)(a => a.toString) == ManningSome("42"))
-      }
+      it("converts the type according to the given function") { assert(actual == ManningSome("42")) }
     }
 
     describe("getOrElse") {
+      val actual1 = s1 getOrElse -1
+      val actual2 = ManningNone getOrElse -1
 
-      it("returns the value, if any") {
-        assert(ManningOption.getOrElse(s1)(-1) == 42)
-      }
-
-      it("returns the default value when no value is available") {
-        assert(ManningOption.getOrElse(ManningNone)(-1) == -1)
-      }
+      it("returns the value, if any") { assert(actual1 == 42) }
+      it("returns the default value when no value is available") { assert(actual2 == -1) }
     }
 
     describe("flatMap") {
+      val actual = s1 flatMap (a => ManningSome(a.toString))
 
-      it("maps and then flats") {
-        assert(ManningOption.flatMap(s1)(a => ManningSome(a.toString)) == ManningSome("42"))
-      }
+      it("maps and then flats") { assert(actual == ManningSome("42")) }
     }
 
     describe("orElse") {
-
       it("executes the function if it's None") {
         assert(ManningOption.orElse(ManningNone)(ManningSome("Yo!")) == ManningSome("Yo!"))
       }
@@ -46,14 +40,11 @@ class TestChapter4 extends FunSpec {
     }
 
     describe("filter") {
+      val actual1 = s1 filter ((a: Int) => a % 2 == 0)
+      val actual2 = s1 filter ((a: Int) => a % 2 != 0)
 
-      it("returns the options if the value does satisfy the condition") {
-        assert(ManningOption.filter(s1)((a: Int) => a % 2 == 0) == s1)
-      }
-
-      it("returns none if the value does NOT satisfy the condition") {
-        assert(ManningOption.filter(s1)((a: Int) => a % 2 != 0) == ManningNone)
-      }
+      it("returns the options if the value does satisfy the condition") { assert(actual1 == s1) }
+      it("returns none if the value does NOT satisfy the condition") { assert(actual2 == ManningNone) }
     }
   }
 }
