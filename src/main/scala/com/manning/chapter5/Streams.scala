e7aa3a7f171618612ac6565fcaa998f8ebc14a6e
Kalimaha <guido.barbaglia@gmail.com>
2016-09-13 19:05:05 +1000

    More on Streams.

--- src/main/scala/com/manning/chapter5/Streams.scala
+++ src/main/scala/com/manning/chapter5/Streams.scala
@@ -29,10 +29,42 @@ object Streams {
         else ManningCons(h, t)
     }
 
-//    def takeWhile(f: A => Boolean): ManningStream[A] = this match {
-//      case ManningEmpty => ManningEmpty
-//      case ManningCons(h, t) => if (f(h())) => ManningStream.cons(h(), t() takeWhile f)
-//    }
+    def takeWhile(f: A => Boolean): ManningStream[A] = this match {
+      case ManningEmpty => ManningEmpty
+      case ManningCons(h, t) =>
+        if (f(h())) ManningStream.cons(h(), t() takeWhile f)
+        else t() takeWhile f
+    }
+
+    def takeWhile2(f: A => Boolean): ManningStream[A] = {
+      foldRight(ManningStream.empty[A])((a, b) =>
+        if (f(a)) ManningStream.cons(a, b)
+        else ManningStream.empty
+      )
+    }
+
+    @tailrec
+    final def exists(f: A => Boolean): Boolean = this match {
+      case ManningEmpty => false
+      case ManningCons(h, t) => f(h()) || t().exists(f)
+    }
+
+    def foldRight[B](z: => B)(f: (A, => B) => B): B = this match {
+      case ManningCons(h,t) => f(h(), t().foldRight(z)(f))
+      case _ => z
+    }
+
+    def exists2(f: A => Boolean): Boolean = foldRight(false)((a, b) => f(a) || b)
+
+    def forAll(f: A => Boolean): Boolean = foldRight(true)((a, b) => f(a) && b)
+
+    def map[B](f: A => B): ManningStream[B] = foldRight(ManningStream.empty[B])((a, b) => ManningStream.cons(f(a), b))
+
+    def filter(f: A => Boolean): ManningStream[A] = foldRight(ManningStream.empty[A])((a, b) => if (f(a)) ManningStream.cons(a, b) else b)
+
+    def append[B >: A](s: => ManningStream[B]): ManningStream[B] = foldRight(s)((h, t) => ManningStream.cons(h, t))
+
+    def flatMap[B](f: A => ManningStream[B]): ManningStream[B] = foldRight(ManningStream.empty[B])((a, b) => f(a) append b)
   }
 
   object ManningStream {
--- src/test/scala/com/manning/chapter5/TestStreams.scala
+++ src/test/scala/com/manning/chapter5/TestStreams.scala
@@ -1,28 +1,23 @@
 package com.manning.chapter5
 
-import com.manning.chapter5.Streams.{ManningCons, ManningEmpty, ManningStream}
+import com.manning.chapter5.Streams.ManningStream
 import org.scalatest.FunSpec
 
 class TestStreams extends FunSpec {
 
   describe("toList") {
-
-    val s = ManningStream(1, 2, 3)
-
     it("converts a Stream to a List") {
-      assert(s.toList == List(1, 2, 3))
+      assert(ManningStream(1, 2, 3).toList == List(1, 2, 3))
     }
   }
 
   describe("take") {
-
     it("takes the first n elements of a stream") {
       assert(ManningStream(1, 2, 3).take(2).toList == List(1, 2))
     }
   }
 
   describe("drop") {
-
     it("skips the first n elements of a stream") {
       assert(ManningStream(1, 2, 3).drop(2).toList == List(3))
     }
@@ -33,11 +28,62 @@ class TestStreams extends FunSpec {
   }
 
   describe("takeWhile") {
-
     it("returns all the elements that match a given predicate") {
-      val f = (i: Int) => i % 2 == 0
-      val s = ManningStream(1, 2, 3, 4, 5)
-      assert(s.takeWhile(f).toList == List(2, 4))
+      assert(ManningStream(1, 2, 3, 4, 5).takeWhile(_ % 2 == 0).toList == List(2, 4))
+    }
+
+//    it("returns all the elements that match a given predicate (foldRight)") {
+//      assert(ManningStream(1, 2, 3, 4, 5).takeWhile2(_ % 2 == 0).toList == List(2, 4))
+//    }
+  }
+
+  describe("exists") {
+    it("asserts whether the Stream contains a given value") {
+      assert(ManningStream(1, 2, 3).exists(_ % 2 == 0))
+      assert(!ManningStream(1, 2, 3).exists(_ % 5 == 0))
+    }
+
+    it("asserts whether the Stream contains a given value (foldRight)") {
+      assert(ManningStream(1, 2, 3).exists2(_ % 2 == 0))
+      assert(!ManningStream(1, 2, 3).exists2(_ % 5 == 0))
+    }
+  }
+
+  describe("foldRight") {
+    it("calculates the sum of a Stream") {
+      assert(ManningStream(1, 2, 3).foldRight(0)(_ + _) == 6)
+    }
+  }
+
+  describe("forAll") {
+    it("checks whether all the elements in the Stream match a given predicate") {
+      assert(ManningStream(1, 2, 3).forAll(_ > 0))
+      assert(!ManningStream(1, 2, 3).forAll(_ < 0))
+    }
+  }
+
+  describe("map") {
+    it("converts the type of a Stream") {
+      assert(ManningStream(1, 2, 3).map(a => a.toString).toList == List("1", "2", "3"))
+    }
+  }
+
+  describe("filter") {
+    it("filters the Stream according to a given predicate") {
+      assert(ManningStream(1, 2, 3).filter(_ > 2).toList == List(3))
+    }
+  }
+
+  describe("append") {
+    it("concatenates two streams") {
+      assert(ManningStream(1, 2, 3).append(ManningStream(4, 5, 6)).toList == List(1, 2, 3, 4, 5, 6))
+    }
+  }
+
+  describe("flatMap") {
+    def f(a: Int): ManningStream[String] = ManningStream(a.toString)
+    it("flatMaps that shit") {
+      assert(ManningStream(1, 2, 3).flatMap(f).toList == List("1", "2", "3"))
     }
   }
 }
