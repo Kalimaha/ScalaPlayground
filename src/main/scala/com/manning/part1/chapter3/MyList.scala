77517b2987da985aeb6925c10ad93697aab1ad9a
Kalimaha <guido.barbaglia@gmail.com>
2016-10-04 19:19:04 +1100

    WIP

--- src/main/scala/com/manning/part1/chapter3/MyList.scala
+++ src/main/scala/com/manning/part1/chapter3/MyList.scala
@@ -1,5 +1,8 @@
 package com.manning.part1.chapter3
 
+import scala.annotation.tailrec
+
+
 sealed trait MyList[+A]
   case object MyNil extends MyList[Nothing]
   case class  MyCons[+A](head: A, tail: MyList[A]) extends MyList[A]
@@ -17,5 +20,36 @@ object MyList {
   def product(l: MyList[Double]): Double = l match {
     case MyNil => 1
     case MyCons(h, t) => h * product(t)
+  }
+
+  def tail[A](l: MyList[A]): MyList[A] = l match {
+    case MyNil => MyNil
+    case MyCons(_, t) => t
+  }
+
+  def tailWithDrop[A](l: MyList[A]): MyList[A] = drop(l)(1)
+
+  def setHead[A](l: MyList[A])(a: A): MyList[A] = l match {
+    case MyNil => MyNil
+    case MyCons(_, t) => MyCons(a, t)
+  }
+
+  def drop[A](l: MyList[A])(n: Int): MyList[A] = {
+    @tailrec
+    def loop(acc: Int)(l: MyList[A]): MyList[A] = {
+      if (acc == n) l
+      else loop(1 + acc)(tail(l))
+    }
+    loop(0)(l)
+  }
+
+  def append[A](l1: MyList[A])(l2: MyList[A]): MyList[A] = l1 match {
+    case MyNil => l2
+    case MyCons(h, t) => MyCons(h, append(t)(l2))
+  }
+
+  def dropWhile[A](f: A => Boolean)(l: MyList[A]): MyList[A] = l match {
+    case MyNil => MyNil
+    case MyCons(h, t) => if (f(h)) dropWhile(f)(t) else MyCons(h, dropWhile(f)(t))
   }
 }
--- src/test/scala/com/manning/part1/chapter3/TestMyList.scala
+++ src/test/scala/com/manning/part1/chapter3/TestMyList.scala
@@ -15,4 +15,46 @@ class TestMyList extends FunSpec {
       assert(product(MyList(1, 2, 3)) == 6)
     }
   }
+
+  describe("tail") {
+    it("returns the tail of a list") {
+      assert(tail(MyList(1, 2, 3)) == MyList(2, 3))
+    }
+
+    it("returns MyNil for an empty list") {
+      assert(tail(MyList()) == MyNil)
+    }
+
+    it("returns the tail of a list (with drop)") {
+      assert(tailWithDrop(MyList(1, 2, 3)) == MyList(2, 3))
+    }
+
+    it("returns MyNil for an empty list (with drop)") {
+      assert(tailWithDrop(MyList()) == MyNil)
+    }
+  }
+
+  describe("setHead") {
+    it("set the head of the list to the given value") {
+      assert(setHead(MyList(1, 2, 3))(4) == MyList(4, 2, 3))
+    }
+  }
+
+  describe("drop") {
+    it("removes the first n elements of a list") {
+      assert(drop(MyList(1, 2, 3, 4, 5))(3) == MyList(4, 5))
+    }
+  }
+
+  describe("append") {
+    it("appends an element to a list") {
+      assert(append(MyList(1, 2, 3))(MyList(4, 5, 6)) == MyList(1, 2, 3, 4, 5, 6))
+    }
+  }
+
+  describe("dropWhile") {
+    it("removes the elements that match a predicate from the list") {
+      assert(dropWhile((i: Int) => i % 2 == 0)(MyList(1, 2, 3, 4, 5, 6)) == MyList(1, 3, 5))
+    }
+  }
 }
