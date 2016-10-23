f09c57bab15ac5926e41a3be4a5a5d46a31e3f59
Kalimaha <guido.barbaglia@gmail.com>
2016-10-23 11:36:56 +1100

    Exceptions WIP.

--- src/main/scala/com/manning/part1/chapter4/MyOption.scala
+++ src/main/scala/com/manning/part1/chapter4/MyOption.scala
@@ -2,12 +2,32 @@ package com.manning.part1.chapter4
 
 sealed trait MyOption[+A] {
   def map[B](f: A => B): MyOption[B]
+  def flatMap[B](f: A => MyOption[B]): MyOption[B]
+  def getOrElse[B >: A](default: => B): B
+  def orElse[B >: A](o: MyOption[B]): MyOption[B]
+  def filter(f: A => Boolean): MyOption[A]
 }
 
 case class MySome[+A](get: A) extends MyOption[A] {
-  def map[B](f: (A) => B): MyOption[B] = MySome(f(this.get))
+  def map[B](f: A => B): MyOption[B] = MySome(f(this.get))
+
+  def flatMap[B](f: A => MyOption[B]): MyOption[B] = f(this.get)
+
+  def getOrElse[B >: A](default: => B): B = this.get
+
+  def orElse[B >: A](o: MyOption[B]): MyOption[B] = MySome(this.get)
+
+  def filter(f: A => Boolean): MyOption[A] = if (f(this.get)) this else MyNone
 }
 
 case object MyNone extends MyOption[Nothing] {
-  def map[B](f: (Nothing) => B): MyOption[B] = MyNone
+  def map[B](f: Nothing => B): MyOption[B] = MyNone
+
+  def flatMap[B](f: Nothing => MyOption[B]): MyOption[B] = MyNone
+
+  def getOrElse[B >: Nothing](default: => B): B = default
+
+  def orElse[B >: Nothing](o: MyOption[B]): MyOption[B] = o
+
+  def filter(f: Nothing => Boolean): MyOption[Nothing] = MyNone
 }
--- src/test/scala/com/manning/part1/chapter4/TestMyOption.scala
+++ src/test/scala/com/manning/part1/chapter4/TestMyOption.scala
@@ -5,11 +5,51 @@ import org.scalatest.FunSpec
 class TestMyOption extends FunSpec {
   describe("map") {
     it("maps a type into another") {
-      assert((MySome(42) map(_.toString)) == MySome("42"))
+      assert((MySome(42) map (_.toString)) == MySome("42"))
     }
 
     it("maps nothing into nothing") {
-      assert((MyNone map(_.toString)) == MyNone)
+      assert((MyNone map (_.toString)) == MyNone)
+    }
+  }
+
+  describe("flatMap") {
+    it("maps and then flats a value") {
+      assert((MySome(42) flatMap ((i: Int) => MySome(i.toString))) == MySome("42"))
+    }
+
+    it("maps and then flats nothing") {
+      assert((MyNone flatMap ((i: Int) => MySome(i.toString))) == MyNone)
+    }
+  }
+
+  describe("getOrElse") {
+    it("returns a value, if any") {
+      assert((MySome(42) getOrElse 0) == 42)
+    }
+
+    it("returns the default value") {
+      assert((MyNone getOrElse 0) == 0)
+    }
+  }
+
+  describe("orElse") {
+    it("returns a value, if any") {
+      assert((MySome(42) orElse MySome(24)) == MySome(42))
+    }
+
+    it("returns the default value") {
+      assert((MyNone orElse MySome(24)) == MySome(24))
+    }
+  }
+
+  describe("filter") {
+    it("returns the option if it does satisfy the predicate") {
+      assert((MySome(42) filter (_ % 2 == 0)) == MySome(42))
+    }
+
+    it("returns nothing if it does not satisfy the predicate") {
+      assert((MySome(42) filter (_ % 2 != 0)) == MyNone)
     }
   }
 }
